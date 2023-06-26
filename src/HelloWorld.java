e0b1b1bf8506de9bd22326944977e4824863f934
kamukotelo <kamukotelo@gmail.com>
2023-06-26 02:54:51 +0100

    Firt commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.time.Clock;
+import java.time.InstantSource;
+import java.time.format.DateTimeFormatter;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+    public class HelloWorld {
+        private static Object InstantSource;
+
+        public static void main(String[] args) {
+            int j = 2023;
+            System.out.println("Hello, World!");
+            System.out.println(j);
+        }
+    }
